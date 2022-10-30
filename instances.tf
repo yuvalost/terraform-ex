7e7a3eac6b78662d901bd15c71e724cbed70528d
Lidor Gerstel <lidorlg@gmail.com>
2022-10-30 18:20:57 +0200

    added all tf files

---
+++ instances.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "linux" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-private-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+  tags = {
+    Name = "linux"
+   
+  }
+}
+
+
+
+resource "aws_instance" "windows" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.large"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-rdp.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+  tags = {
+    Name = "windows"
+   
+  }
+}
---
+++ sg.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "allow-ssh" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-ssh"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_instance.windows.public_ip]
+  }
+  tags = {
+    Name = "allow-ssh"
+  }
+}
+
+
+
+resource "aws_security_group" "allow-rdp" {
+  vpc_id      = aws_vpc.main.id
+  name        = "allow-rdp"
+  description = "security group that allows rdp and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-rdp"
+  }
+}
